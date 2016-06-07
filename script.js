66759a0d4f107f29e0410a14334d2966453bc341
Lisa Vogt <lisavogt.sf@gmail.com>
2016-06-06 22:05:48 -0700

    displays fake thumbnails in flexbox

--- script.js
+++ script.js
@@ -2,6 +2,15 @@
 document.addEventListener('DOMContentLoaded', function () {
 
 	// get thumbnail images from API and posted to screen
+	var thumbnails = document.querySelector('#thumbnails');
+	var newThumbnail;
+
+	for (var i = 0; i < 20; i++) {
+		console.log("i?", i);
+		newThumbnail = document.createElement("div");
+		newThumbnail.classList.add('mini');
+		thumbnails.appendChild(newThumbnail);
+	}
 
 	// set up an event listener for each thumbnail
 		// to launch viewer
