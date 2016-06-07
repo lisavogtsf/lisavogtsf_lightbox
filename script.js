4d703d17cef5589848b76e816e9d0cbe8bb947d2
Lisa Vogt <lisavogt.sf@gmail.com>
2016-06-06 18:27:17 -0700

    outlines js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+
+	// get thumbnail images from API and posted to screen
+
+	// set up an event listener for each thumbnail
+		// to launch viewer
+
+	// set up event listeners on viewer to look at next thumbnail, previous, close
+});
