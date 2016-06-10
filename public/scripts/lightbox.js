0974315d0f3f090186e2258a6ed04e268bf7b4e4
Lisa Vogt <lisavogt.sf@gmail.com>
2016-06-09 22:08:18 -0700

    viewer frame nicely covers thumbnails, disappears on click

--- public/scripts/lightbox.js
+++ public/scripts/lightbox.js
@@ -1,17 +1,36 @@
 // scripts for lightbox page dom interaction
 console.log("lightbox.js");
 
-function openLightbox (e) {
+function openLightbox (e, photoUrls) {
 
-	var thumbnailBackgroundImage = e.target.style.backgroundImage;
+	// gather elements
 	var viewerFrame = document.querySelector('#viewer-frame');
+	var viewer = document.querySelector('#viewer');
+	var thumbnailFrame = document.querySelector('#thumbnail-frame');
+	
+	// gather thumbnail background/order
+	var thumbnailBackgroundImage = e.target.style.backgroundImage;
 	var photoOrder = e.target.dataset.order;
+
+	// give viewer correct thumbnail order and background
+	viewer.style.backgroundImage = thumbnailBackgroundImage;
+	viewer.dataset.order = photoOrder;
+
+	// show whole frame
+	console.log(thumbnailFrame.getBoundingClientRect());
+	viewerFrame.style.height = thumbnailFrame.getBoundingClientRect().height + "px";
 	viewerFrame.classList.remove('hide');
 }
 
 function closeLightbox () {
-	console.log("closing");
+
+	// gather elements
 	var viewerFrame = document.querySelector('#viewer-frame');
-	viewerFrame.style.backgroundImage = url();
+	var viewer = document.querySelector('#viewer');
+
+	// hide whole frame
 	viewerFrame.classList.add('hide');
+	// clear out viewer
+	viewer.style.backgroundImage = "";
+	viewer.dataset.order = "";
 }
