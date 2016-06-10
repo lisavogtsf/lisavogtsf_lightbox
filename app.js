8ba5bf62a592e4b1cab546e8e9c26176c1a9158e
Lisa Vogt <lisavogt.sf@gmail.com>
2016-06-09 18:39:25 -0700

    retreiieves photos from flickr search

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var ejs = require('ejs');
 var bodyParser = require('body-parser');
+var XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+var xhr = new XMLHttpRequest();
 var config = require('./config');
 
 var app = express();
 
-var flickrApiKey = config('FLICKR_KEY');
-console.log("flickrApiKey", flickrApiKey);
 
 // set up view engine
 app.set('view engine', 'ejs');
@@ -15,7 +15,32 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-	res.render('index');
+
+	// leave open for other searches in the future
+	var defaultSearchTerm = "cats";
+	var searchTerm = searchTerm || defaultSearchTerm;
+	var defaultSearchLimit = 25;
+
+	// put together search url
+	var flickrSearchUrl = "https://www.flickr.com/services/rest/?method=flickr.photos.search&format=json";
+	flickrSearchUrl += "&api_key=" + config('FLICKR_KEY');
+	flickrSearchUrl += "&limit=" + defaultSearchLimit;
+	flickrSearchUrl += "&nojsoncallback=1";
+	flickrSearchUrl += "&text=" + searchTerm;
+
+	// search flickr api for photos matching term
+	xhr.open('GET', flickrSearchUrl);
+	xhr.onreadystatechange = function () {
+		if ((xhr.readyState === 4) && (xhr.status === 200)) {
+			var data = JSON.parse(xhr.responseText);
+			var photos = data.photos.photo;
+			console.log("photos", photos);
+			// send those photos to render
+			res.render('index', {photos: photos});
+		}
+	};
+	xhr.send();
+
 });
 
 app.listen((config('PORT') || 3000), function (err) {
